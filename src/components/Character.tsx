72e962782a042a11b6a7b819fe70754c2f17874c
Jean-Baptiste CAMBILLARD <jeanbaptiste.cambillard@gmail.com>
2023-07-17 20:10:23 +0200

    spacing

--- src/components/Character.tsx
+++ src/components/Character.tsx
@@ -16,8 +16,8 @@ function Character({ character, detailsPage }: IProps) {
           <h2 className="character__name font-bold">{character.name}</h2>
           <h3 className="character__role">{character.role}</h3>
           <h4 className="character_dicton mb-5">"{character.dicton}"</h4>
-          <p className="character__description">{character.description}</p>
-
+          <p className="character__description text-justify">{character.description}</p>
+          
           <Link to={`/character/${character.id}`}>
             <button className="moreinfo-btn mt-10 font-bold">
               En savoir plus
