de25e100bc4846a4cf9bf1e9057300d4817d03ba
Jean-Baptiste CAMBILLARD <jeanbaptiste.cambillard@gmail.com>
2023-07-19 17:39:20 +0200

    update : modify buttons

--- src/components/CharacterInformation.tsx
+++ src/components/CharacterInformation.tsx
@@ -1,27 +1,109 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
-
 
 function CharacterInformation() {
   const location = useLocation();
-  const { character } = location.state;
+  const [editMode, setEditMode] = useState(false);
+  const [editedCharacter, setEditedCharacter] = useState(location.state.character);
+
+
+  const handleEditClick = () => {
+    setEditMode(true);
+  };
+
+  const handleSaveClick = () => {
+    // Effectuez ici les actions nécessaires pour enregistrer les modifications
+    // Par exemple, vous pouvez appeler une fonction qui met à jour les informations du personnage dans votre backend
+
+    setEditMode(false);
+    // Ajoutez ici le code pour enregistrer les modifications
+  };
+
+  const handleCancelClick = () => {
+    setEditMode(false);
+    setEditedCharacter(location.state.character);
+  };
+
+  const handleInputChange = (event: { target: { name: any; value: any; }; }) => {
+    const { name, value } = event.target;
+    setEditedCharacter((prevCharacter: any) => ({
+      ...prevCharacter,
+      [name]: value,
+    }));
+  };
 
   return (
     <>
-    <h1 className="title mb-5 mt-8">Détail du personnage</h1>
-    <div className="character-preview">
-      <div className="flex">
-        <img src={character.avatar} className="character-image" />
-        <div className="">
-          <h2 className="character__name font-bold">{character.name}</h2>
-          <h3 className="character__role">{character.role}</h3>
-          <h4 className="character_dicton mb-5">"{character.dicton}"</h4>
-          <p className="character__description text-justify">{character.description}</p>
+      <h1 className="title mb-5 mt-8">Détail du personnage</h1>
+      <div className="character-preview">
+        <div className="flex flex-col">
+          <img src={editedCharacter.avatar} className="character-image mx-auto" />
+          <div className="character-details">
+            {editMode ? (
+              <>
+                <div className="input-group">
+                  <label>Nom:</label>
+                  <input
+                    type="text"
+                    name="name"
+                    value={editedCharacter.name}
+                    onChange={handleInputChange}
+                    className="character__name font-bold"
+                  />
+                </div>
+                <div className="input-group">
+                  <label>Rôle:</label>
+                  <input
+                    type="text"
+                    value={editedCharacter.role}
+                    onChange={handleInputChange}
+                    className="character__role"
+                  />
+                </div>
+                <div className="input-group">
+                  <label>Dicton:</label>
+                  <textarea
+                    name="dicton"
+                    value={editedCharacter.dicton}
+                    onChange={handleInputChange}
+                    className="character_dicton"
+                  />
+                </div>
+                <div className="input-group">
+                  <label>Description:</label>
+                  <textarea
+                    name="description"
+                    value={editedCharacter.description}
+                    onChange={handleInputChange}
+                    className="character__description text-justify"
+                  />
+                </div>
+              </>
+            ) : (
+              <>
+                <h2 className="character__name font-bold">{editedCharacter.name}</h2>
+                <h3 className="character__role">{editedCharacter.role}</h3>
+                <h4 className="character_dicton mb-5">"{editedCharacter.dicton}"</h4>
+                <p className="character__description text-justify">{editedCharacter.description}</p>
+              </>
+            )}
+          </div>
         </div>
+        {!editMode && (
+          <strong className="edit-text" onClick={handleEditClick}>
+            Modifier les informations
+          </strong>
+        )}
+        {editMode && (
+          <div>
+            <button className="moreinfo-btn mr-10" onClick={handleSaveClick}>Enregistrer</button>
+            <button className="moreinfo-btn" onClick={handleCancelClick}>Annuler</button>
+          </div>
+        )}
       </div>
-    </div>
     </>
   );
 }
