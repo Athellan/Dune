6d89c2cd5feee36d7bb9674a018370a88c0e2129
Jean-Baptiste CAMBILLARD <jeanbaptiste.cambillard@gmail.com>
2023-07-22 18:48:47 +0200

    responsive card

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -14,7 +14,7 @@ function Character({ character }: IProps) {
 
   const navigate = useNavigate();
   return (
-    <div className="character-preview !my-10">
+    <div className="character-preview !my-10" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
       <div className="flex">
         <img src={character.avatar} className="character-image" />
         <div className="">
@@ -22,22 +22,27 @@ function Character({ character }: IProps) {
           <h3 className="character__role">{character.role}</h3>
           <h4 className="character_dicton mb-5">"{character.dicton}"</h4>
           <p className="character__description text-justify">{character.description}</p>
-          <div className="">
-            <Link to={`/character/${character.id}`} state={{ character: character }}>
-              <button className="moreinfo-btn mt-10 font-bold">
-                En savoir plus
-              </button>
-            </Link>
-            <button
-              onClick={() => deleteCharacter(character.id, navigate)}
-              className="inline-block ml-14 px-3 py-1 text-white font-bold bg-red-500 rounded-lg hover:bg-red-600 focus:outline-none focus:ring focus:ring-red-300"
-            >
-              Supprimer
-            </button>
-          </div>
         </div>
       </div>
-    </div >
+      <div className="flex justify-between">
+        <div className="">
+          <Link to={`/character/${character.id}`} state={{ character: character }}>
+            <button className="moreinfo-btn ml-32 mt-10 font-bold">
+              En savoir plus
+            </button>
+          </Link>
+        </div>
+        <div className="">
+          <button
+            onClick={() => deleteCharacter(character.id, navigate)}
+            className="inline-block mt-4 px-3 py-1 text-white font-bold bg-red-500 rounded-lg hover:bg-red-600 focus:outline-none focus:ring focus:ring-red-300"
+          >
+            Supprimer
+          </button>
+        </div>
+      </div>
+    </div>
+
   );
 }
 
