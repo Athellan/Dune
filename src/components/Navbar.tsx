fd4dea42755e9e1c06f51d2fc29f4ac155d7c6de
Jean-Baptiste CAMBILLARD <jeanbaptiste.cambillard@gmail.com>
2023-09-06 20:04:13 +0200

    adjustements

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -35,7 +35,7 @@ function Navbar() {
           to="/newcharacter"
           onClick={() => handleButtonClick("nouveau")}
         >
-          Nouveau Personnage
+          Nouveaux Personnage
         </Link>
         <Link
           style={{
